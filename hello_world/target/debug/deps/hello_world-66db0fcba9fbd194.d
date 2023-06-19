26f4af91262ba1bc66fb568abc8aa8dea70a2327
Gary Mejia <gmejima@ucsc.edu>
2023-06-19 15:09:59 -0700

    Removed target directory

--- hello_world/target/debug/deps/hello_world-66db0fcba9fbd194.d
+++
@@ -1,5 +0,0 @@
-/Users/gmm117/Documents/rust/rust_learning/hello_world/target/debug/deps/hello_world-66db0fcba9fbd194: src/main.rs
-
-/Users/gmm117/Documents/rust/rust_learning/hello_world/target/debug/deps/hello_world-66db0fcba9fbd194.d: src/main.rs
-
-src/main.rs:
--- hello_world/target/debug/deps/hello_world-c471664e93eae1df.d
+++
@@ -1,5 +0,0 @@
-/Users/gmm117/Documents/rust/rust_learning/hello_world/target/debug/deps/hello_world-c471664e93eae1df.rmeta: src/main.rs
-
-/Users/gmm117/Documents/rust/rust_learning/hello_world/target/debug/deps/hello_world-c471664e93eae1df.d: src/main.rs
-
-src/main.rs:
